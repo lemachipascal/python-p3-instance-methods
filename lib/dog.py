75a5b04f1f728952d31192691990caca855703ce
Lemachipascal-dev <lemachijo421@gmail.com>
2024-09-11 11:49:45 +0300

    instance method

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-
+#Add an instance method sit() to your Dog class that will print "The dog is sitting."
 class Dog:
     # Class body goes here
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def roll_over(self):
+        print("The dog rolled over!")
+
+   
+   
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-
+#Add an instance method talk() to your Person class that will print "Hello World!"
+# Add an instance method walk() to your Person class that will print "The person is walking."
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+
+
+   
